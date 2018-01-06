240dbda2f30f6c44b24768cadc9d266939957623
Shoma SATO <noir.neo.04@gmail.com>
2018-01-06 23:14:20 +0900

    Implemont Player Mover

---
+++ Assets/Scripts/Players/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Players
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        [Inject]
+        void Initialize(List<IInputEventProvider> inputEventProviders)
+        {
+            inputEventProviders.Select(x => x.MoveDirection)
+                .Merge()
+                .Subscribe(Move)
+                .AddTo(this);
+        }
+
+        private void Move(Vector3 direction)
+        {
+            var pos = transform.position;
+            pos += direction;
+            transform.position = pos;
+        }
+    }
+}
