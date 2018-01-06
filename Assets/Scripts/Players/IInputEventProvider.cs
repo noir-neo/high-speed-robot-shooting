1af1134d8863d35052c238719f6d1a183d31a068
Shota HAMADA <maku693@outlook.com>
2018-01-07 05:07:06 +0900

    Separate file for InputValue and PlayerId class

--- Assets/Scripts/Players/IInputEventProvider.cs
+++ Assets/Scripts/Players/IInputEventProvider.cs
@@ -3,24 +3,6 @@ using UnityEngine;
 
 namespace Players
 {
-    public enum PlayerId
-    {
-        Player1 = 1,
-        Player2 = 2,
-    }
-
-    public class InputValue<T>
-    {
-        public PlayerId PlayerId;
-        public T Value;
-
-        public InputValue(PlayerId playerId, T value)
-        {
-            PlayerId = playerId;
-            Value = value;
-        }
-    }
-
     interface IInputEventProvider
     {
         IObservable<Vector3> MoveDirection(PlayerId playerId);
