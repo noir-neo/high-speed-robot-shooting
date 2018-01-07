b2cc6d6a2a7aaae6fe74ad6d291417535f55ea7b
Shoma SATO <noir.neo.04@gmail.com>
2018-01-07 16:51:30 +0900

    Fire!!!

---
+++ Assets/Scripts/Bullets/BulletImpls/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Bullets.BulletImpls
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Bullet : MonoBehaviour, IBullet
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _destroyTime;
+
+        public void Fire(Vector3 forward)
+        {
+            _rigidbody.AddForce(forward * _speed, ForceMode.Impulse);
+            StartCoroutine(Destroy());
+        }
+
+        private IEnumerator Destroy()
+        {
+            yield return new WaitForSeconds(_destroyTime);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Weapons/WeaponImpls/Weapon.cs
+++ Assets/Scripts/Weapons/WeaponImpls/Weapon.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
+using Zenject.SpaceFighter;
+using Bullet = Bullets.BulletImpls.Bullet;
 
 namespace Weapons.WeaponImpls
 {
     public class Weapon : MonoBehaviour, IWeapon
     {
+        [SerializeField] private Bullet _bulletPrefab;
         [SerializeField] private FollowTransform _followTransform;
+        [SerializeField] private Transform _muzzle;
+
         public void Configure(Transform root)
         {
             _followTransform.target = root;
         }
 
-        public void Shoot()
+        void IWeapon.Shoot()
         {
-
+            // HACK: Trail 出ちゃうので
+            var bullet = Instantiate(_bulletPrefab, _muzzle);
+            bullet.transform.SetParent(null);
+            bullet.Fire(transform.forward);
         }
     }
 }
