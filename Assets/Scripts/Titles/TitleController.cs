fbf81c5a7eac645968e1248b4e68a64ad678ee7a
Shoma SATO <noir.neo.04@gmail.com>
2018-01-07 21:14:18 +0900

    Title

---
+++ Assets/Scripts/Titles/TitleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Titles
+{
+    public class TitleController : MonoBehaviour
+    {
+        void Awake()
+        {
+            Observable.Timer(TimeSpan.FromSeconds(2))
+                .ContinueWith(_ => SceneManager.LoadSceneAsync("Main", LoadSceneMode.Additive).AsObservable())
+                .ContinueWith(_ => SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene()).AsObservable())
+                .Subscribe()
+                .AddTo(this);
+        }
+    }
+}
