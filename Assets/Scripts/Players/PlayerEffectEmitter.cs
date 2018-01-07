d150ed1f4e044febfba60859f95785e029dcddbd
Shoma SATO <noir.neo.04@gmail.com>
2018-01-07 19:01:24 +0900

    Add EffectEmmiter

---
+++ Assets/Scripts/Players/PlayerEffectEmitter.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Players
+{
+    public class PlayerEffectEmitter : MonoBehaviour
+    {
+        [SerializeField] private PlayerCore _playerCore;
+        [SerializeField] private ParticleSystem _exploded;
+        [SerializeField] private ParticleSystem _damaged;
+
+        void Start()
+        {
+            _playerCore.Explode
+                .Subscribe(_ => _exploded.Play())
+                .AddTo(this);
+            _playerCore.Damage
+                .Subscribe(_ => _damaged.Play())
+                .AddTo(this);
+        }
+    }
+}
