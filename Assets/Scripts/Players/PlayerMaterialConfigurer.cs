a75bc0fd6a8b93137b1f62b996dfda6b5dd25ee4
Shoma SATO <noir.neo.04@gmail.com>
2018-01-07 14:32:08 +0900

    Player の色を自動で設定する

---
+++ Assets/Scripts/Players/PlayerMaterialConfigurer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Players
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Renderer))]
+    public class PlayerMaterialConfigurer : MonoBehaviour
+    {
+        [SerializeField] private PlayerCore _playerCore;
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] private PlayerMaterialSetting[] _materials;
+
+        [Serializable]
+        class PlayerMaterialSetting
+        {
+            [SerializeField] private PlayerId _playerId;
+            public PlayerId PlayerId => _playerId;
+            [SerializeField] private Material _material;
+            public Material Material => _material;
+        }
+
+        void OnEnable()
+        {
+            ConfigureMaterial();
+        }
+
+        void OnValidate()
+        {
+            ConfigureMaterial();
+        }
+
+        private void ConfigureMaterial()
+        {
+            _renderer.material = _materials.First(m => m.PlayerId == _playerCore.PlayerId).Material;
+        }
+    }
+}
